31545cb8fcd77c1f8fe27f98c8bb0fd5b76e7b24
kenjid4641 <kenjid2789@nycstudents.net>
2024-09-27 14:13:09 -0400

    Updated script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Kenji Denaro"; // update the part BETWEEN the "quotes"
+let userName = "Kenjid4641"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
